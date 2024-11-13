21b5bfa730d16bce3be39cb44da5cc2ec5de42f0
DINIGMA <alex.gnedoffff@mail.ru>
2024-11-14 00:05:35 +0800

    add project structure

---
+++ frontend/src/app/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>Hello</div>
+</template>
+
+<style>
+@import "./styles/main.css"
+</style>
---
+++ frontend/src/app/index.ts
@@ -0,0 +1,15 @@
+
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import {router, store} from './providers'
+
+const initializeApp = createApp(App)
+
+initializeApp.use(store).use(router)
+
+export const app = initializeApp
+
+
+
+
